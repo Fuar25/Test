5cf41b7a1faf9138666a4196cd3a95da2a748850
Fuar25 <150438078+Fuar25@users.noreply.github.com>
2024-09-01 19:12:22 +0800

    Update Classification.py

--- Classification.py
+++ Classification.py
@@ -4,20 +4,24 @@ def main():
     # 设置页面标题
     st.title("在线视频播放器")
 
-    col1, col2 = st.columns(2)
+    col1, col2, col3 = st.columns([1, 4, 1])
 
     with col1:
     # 上传视频文件
         video_file = st.file_uploader("Upload a video file", type=["mp4", "avi", "mov"])
     with col2:
+        video_container = st.empty()
+    with col3:
         play_video = st.button("播放视频")
+        if not play_video:
+            st.write("按下按钮以播放视频")
     
     if video_file is not None:
         if play_video:
         # 直接在 Streamlit 中显示视频
-            st.video(video_file)
+            video_container.video(video_file)
         else:
-            st.write("按下按钮以播放视频")
+            video_container.empty()
             
 if __name__ == "__main__":
     main()
