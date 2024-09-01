2d551ac8fe6e331c22848eb959295c7d58f8ddd7
Fuar25 <150438078+Fuar25@users.noreply.github.com>
2024-09-01 19:02:04 +0800

    Update Classification.py

--- Classification.py
+++ Classification.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
     # 设置页面标题
-    st.title("Video Player App")
+    st.title("在线视频播放器")
 
     # 上传视频文件
     video_file = st.file_uploader("Upload a video file", type=["mp4", "avi", "mov"])
